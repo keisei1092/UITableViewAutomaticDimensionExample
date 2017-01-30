cd4b29be6cb6c45bfa82dec070c97ad7953468ee
keisei_1092 <keisei_1092@yahoo.co.jp>
2017-01-30 16:30:43 +0900

    Initial Commit

---
+++ UITableViewPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITableViewPractice
+//
+//  Created by SaitoKeisei on 2017/1/30.
+//  Copyright © 2017年 keisei_1092. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
